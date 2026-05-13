a9e23bbb9b21c13971227811995391273fddd23b
Create093 <gilbert.tzai.alt.alt@gmail.com>
2026-05-13 20:11:40 +0700

    function.js

--- function.js
+++ function.js
@@ -1,28 +1,39 @@
 const choices = document.querySelectorAll(".choice");
 const resultText = document.getElementById("result");
+
 const computerChoices = ["rock", "paper", "scissors"];
+
 for (let i = 0; i < choices.length; i++) {
     choices[i].addEventListener("click", function () {
+
         const playerChoice = choices[i].id;
+
         const randomIndex = Math.floor(Math.random() * 3);
         const computerChoice = computerChoices[randomIndex];
+
         let result = "";
 
-        if KALAU DRAW {
+        if (playerChoice === computerChoice) {
             result = "DRAW!";
         }
+
+
         else if (
-            (KALAU PLAYER BATU DAN KOMPUTER GUNTING) ||
-            (KALAU PLAYER KERTAS DAN KOMPUTER BATU) ||
-            (KALAU PLAYER GUNTING DAN KOMPUTER KERTAS)
+            (playerChoice === "rock" && computerChoice === "scissors") ||
+            (playerChoice === "paper" && computerChoice === "rock") ||
+            (playerChoice === "scissors" && computerChoice === "paper")
         ) {
             result = "YOU WIN!";
         }
-            
+
+        else {
+            result = "YOU LOSE!";
+        }
+
         resultText.innerHTML = `
             You picked: <strong>${playerChoice}</strong><br>
             Computer picked: <strong>${computerChoice}</strong><br><br>
-            <h2>${result}</h2>
+           <h2>${result}</h2>
         `;
     });
 }
