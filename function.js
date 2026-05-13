ba31dd83e95d492d829ea8b86a04ce55a6ab94d7
Create093 <gilbert.tzai.alt.alt@gmail.com>
2026-05-13 19:56:51 +0700

    function.js

--- function.js
+++ function.js
@@ -1,1 +1,28 @@
+const choices = document.querySelectorAll(".choice");
+const resultText = document.getElementById("result");
+const computerChoices = ["rock", "paper", "scissors"];
+for (let i = 0; i < choices.length; i++) {
+    choices[i].addEventListener("click", function () {
+        const playerChoice = choices[i].id;
+        const randomIndex = Math.floor(Math.random() * 3);
+        const computerChoice = computerChoices[randomIndex];
+        let result = "";
 
+        if KALAU DRAW {
+            result = "DRAW!";
+        }
+        else if (
+            (KALAU PLAYER BATU DAN KOMPUTER GUNTING) ||
+            (KALAU PLAYER KERTAS DAN KOMPUTER BATU) ||
+            (KALAU PLAYER GUNTING DAN KOMPUTER KERTAS)
+        ) {
+            result = "YOU WIN!";
+        }
+            
+        resultText.innerHTML = `
+            You picked: <strong>${playerChoice}</strong><br>
+            Computer picked: <strong>${computerChoice}</strong><br><br>
+            <h2>${result}</h2>
+        `;
+    });
+}
